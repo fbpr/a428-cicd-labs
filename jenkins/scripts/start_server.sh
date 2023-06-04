6f48882b4da45c04b858a24a262e127aa4cc2728
fbpr <71777049+fbpr@users.noreply.github.com>
2023-06-04 12:02:15 +0700

    update start_server cmd

--- jenkins/scripts/start_server.sh
+++ jenkins/scripts/start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /var/www/src
 sudo npm run build
-sudo pm2 start npm -- run production
+sudo pm2 start npm --name "my-app" -- start
+sudo pm2 startup
+sudo pm2 save
+sudo pm2 restart all
